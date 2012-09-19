d855225b832cc46a57d4030c7870fc2b5d64bf78
Ben Thomasson <ben.thomasson@gmail.com>
2012-09-19 16:05:45 -0400

    Adding AUTHORIZATION_KEY checking to access robots

--- treeserver/leaf/api/resources.py
+++ treeserver/leaf/api/resources.py
@@ -6,6 +6,25 @@ from django.contrib.auth.models import User
 
 from tastypie.authentication import BasicAuthentication
 from tastypie.authorization import DjangoAuthorization
+import functools
+
+
+class ObjectAuthorization(DjangoAuthorization):
+
+    def __init__(self, auth_key, object_key):
+        self.auth_key = auth_key
+        self.object_key = object_key
+
+    def apply_limits(self, request, object_list):
+        print request.META
+        print object_list
+        return filter(functools.partial(self.test_auth_key, request), object_list)
+
+    def test_auth_key(self, request, obj):
+        if not hasattr(obj, self.object_key):
+            return True
+        else:
+            return request.META.get(self.auth_key) == getattr(obj, self.object_key)
 
 
 class RobotResource(ModelResource):
@@ -14,7 +33,8 @@ class RobotResource(ModelResource):
         queryset = Robot.objects.all()
         allowed_methods = ['get']
         authentication = BasicAuthentication(realm="")
-        authorization = DjangoAuthorization()
+        authorization = ObjectAuthorization('HTTP_AUTHORIZATION_KEY', 'authorization')
+
 
 class ConfigurationResource(ModelResource):
 
@@ -26,5 +46,5 @@ class ConfigurationResource(ModelResource):
         authentication = BasicAuthentication(realm="")
         authorization = DjangoAuthorization()
         filtering = {
-            "robot": ('exact',),
+            "robot": ('exact', ),
         }
--- treeserver/leaf/models.py
+++ treeserver/leaf/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 from common.fields import UUIDField
 
+import random
+import hashlib
+
+def generate_authorization():
+    return hashlib.sha256( str(random.getrandbits(256)) ).hexdigest()
 
 class Robot(models.Model):
 
     uuid = UUIDField(primary_key=True)
+    authorization = models.CharField(max_length=64, default=generate_authorization, blank=True)
 
     def __str__(self):
         return self.uuid
